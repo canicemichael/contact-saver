0eb6a0d85bccbc5dbabfe12f3f84fd05ccf78ca6
canicemichael <canicemichael@gmail.com>
2024-02-22 17:04:02 +0100

    create contact pages

---
+++ public/js/home.js
@@ -0,0 +1,10 @@
+// JavaScript to handle button click
+document.getElementById("myButton").addEventListener("click", function () {
+  // Redirect to another page
+  window.location.href = "http://localhost:5003/login";
+});
+
+document.getElementById("myRegister").addEventListener("click", function () {
+    // Redirect to another page
+    window.location.href = "http://localhost:5003/register";
+  });

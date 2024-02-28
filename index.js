3b0ff940918dc49b47ff2673461181ab116cc3d8
canicemichael <canicemichael@gmail.com>
2024-02-28 11:36:26 +0100

    completed login route function using passport

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnection");
+const session = require("express-session");
 const cors = require("cors");
 const path = require("path");
 const flash = require("connect-flash");
 const passport = require("passport");
+const MongoStore = require("connect-mongo");
 const dotenv = require("dotenv").config();
 
 const asyncHandler = require("express-async-handler");
-const {User} = require("./models/userModel");
+const { User } = require("./models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -17,7 +19,7 @@ const funct = require("./config/funct");
 
 // importing functions
 let verification_code = funct.verification_code,
-reset_code = funct.reset_code;
+  reset_code = funct.reset_code;
 
 const port = process.env.PORT;
 
@@ -35,6 +37,72 @@ app.use(errorHandler);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
+    secret: "xxx secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.CONNECTION_STR,
+      useUnifiedTopology: true,
+      ttl: 1 * 24 * 60 * 60,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// flash
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// Auth Setup
+const localStrategy = require("passport-local").Strategy;
+
+// passport config
+passport.use(
+  new localStrategy(
+    { usernameField: "email", passReqToCallback: true },
+    (req, email, password, done) => {
+      // Match user
+      User.findOne({
+        email: email,
+      })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: "Invalid Credentials, Try Again",
+            });
+          }
+          if (user.password != password) {
+            return done(null, false, { message: "Incorrect Password" });
+          }
+          return done(null, user);
+        })
+        .catch((err) => {
+          return res.redirect("/");
+        });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 app.use(express.static("public"));
 
@@ -135,9 +203,9 @@ app.get(
 // );
 
 app.post("/register", (req, res) => {
-  const { username, email, password, password2 } = req.body;
+  const { email, password, password2 } = req.body;
   let errors = [];
-  if (!username || !email || !password || !password2) {
+  if (!email || !password || !password2) {
     errors.push({ mes: "Please enter all fields" });
     console.log(errors);
   }
@@ -148,7 +216,7 @@ app.post("/register", (req, res) => {
     errors.push({ msg: "Password must be at least 8 characters" });
   }
   if (errors.length > 0) {
-    res.render("register", {
+    res.render("auth/register", {
       errors,
       email,
       password,
@@ -169,7 +237,7 @@ app.post("/register", (req, res) => {
           .then((new_user) => {
             // welcome_mail(new_user);
             req.flash("success_msg", "Account created, Please login");
-            return res.redirect("/login");
+            return res.render("auth/login");
           })
           .catch((err) => {
             errors.push({ msg: "Something went wrong" });
@@ -194,35 +262,54 @@ app.get(
 //@access public
 app.post(
   "/login",
-  asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400);
-      throw new Error("All fields are mandatory!");
-    }
-    const user = await User.findOne({ email });
-    // compare password with hashedpassword
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const accessToken = jwt.sign(
-        {
-          user: {
-            username: user.username,
-            email: user.email,
-            id: user.id,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "1d" }
-      );
-      // res.status(200).json({ accessToken });
-      // save the tokens local storage or something
-      return res.render("contact/contact_query");
-    } else {
-      res.status(401);
-      throw new Error("email or password is not valid");
-    }
-    res.json({ message: "Login user" });
-  })
+  // asyncHandler(async (req, res) => {
+  //   const { email, password } = req.body;
+  //   if (!email || !password) {
+  //     res.status(400);
+  //     throw new Error("All fields are mandatory!");
+  //   }
+  //   const user = await User.findOne({ email });
+  //   // compare password with hashedpassword
+  //   if (user && (await bcrypt.compare(password, user.password))) {
+  //     const accessToken = jwt.sign(
+  //       {
+  //         user: {
+  //           username: user.username,
+  //           email: user.email,
+  //           id: user.id,
+  //         },
+  //       },
+  //       process.env.ACCESS_TOKEN_SECRET,
+  //       { expiresIn: "1d" }
+  //     );
+  //     // res.status(200).json({ accessToken });
+  //     // save the tokens local storage or something
+  //     return res.render("contact/contact_query");
+  //   } else {
+  //     res.status(401);
+  //     throw new Error("email or password is not valid");
+  //   }
+  //   res.json({ message: "Login user" });
+  // })
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        // res.redirect("/" + user._id);
+        res.redirect("/contactPage");
+      })
+      .catch((err) => {
+        req.flash(
+          "error_msg",
+          "Invalid Credentials! If error persists, contact support."
+        );
+        console.log(err);
+        return res.redirect("back");
+      });
+  }
 );
 
 // ============= contact routes =================
